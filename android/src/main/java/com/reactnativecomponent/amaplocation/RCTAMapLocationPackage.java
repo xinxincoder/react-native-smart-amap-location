688f6a25956e107e6a45e98b5e5902ddce44cbcd
lxx <xinxincoder@163.com>
2018-06-09 17:53:00 +0800

    去掉安卓不能编译

--- android/src/main/java/com/reactnativecomponent/amaplocation/RCTAMapLocationPackage.java
+++ android/src/main/java/com/reactnativecomponent/amaplocation/RCTAMapLocationPackage.java
@@ -20,7 +20,6 @@ public class RCTAMapLocationPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
